c7b67d2eca53a652a8cba93c480a7c519dc9c3fe
zjkwdy <3377911508@qq.com>
2022-07-14 11:32:43 +0800

    df

--- functions/softs/softs.ts
+++
@@ -1,9 +0,0 @@
-export default {
-    async fetch(request, env) {
-        let url = new URL(request.url);
-        url.hostname = 'hakurei-reimu.run.goorm.io'
-        url.pathname = '/softs/'
-        let new_request = new Request(url);
-        return fetch(new_request);
-    },
-};  
